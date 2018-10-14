08609b00e12f8d7d6a03e142f0fddb55a8163cd4
DESKTOP-U046IK5\Thomas <pellthomas@hotmail.com>
2018-10-14 15:58:34 -0400

    numero3

---
+++ Question3/Assets/Scripts/SupprimerTexte.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SupprimerTexte : MonoBehaviour {
+
+    public Text texte;
+    public float temps = 2;
+	// Use this for initialization
+	void Start () {
+        ChangerTexte();
+        DetruireTexte();
+	}
+
+    void ChangerTexte()
+    {
+        texte.text = "Bang";
+    }
+
+    void DetruireTexte()
+    {
+        Destroy(gameObject, temps);
+    }
+}
